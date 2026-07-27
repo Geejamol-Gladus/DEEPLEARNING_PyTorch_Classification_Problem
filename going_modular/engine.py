c4b5d0b016868aaccfb7807204e546ec828c9ac0
GMG <geejamolg@gmail.com>
2026-07-27 15:24:23 +0400

    finished teh engine of Deeplearning

---
+++ going_modular/engine.py
@@ -0,0 +1,102 @@
+import torch
+from torch import nn
+import mlflow 
+
+def train_setup(model,train_dataloader,loss_fn,optimizer):
+    model.train()
+    train_loss=0
+    total_sample=0
+    total_correct =0
+    for X_batch,y_batch in train_dataloader:
+        # send them to target device 
+        # do the forward pass
+         
+        logits =model(X_batch).squeeze(1)
+
+        # calaulate teh loss
+
+        loss =loss_fn(logits,y_batch)
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        # get teh number of samples in current batch
+
+        batch_size =X_batch.size(0)
+
+        #  calulate the average 
+        train_loss+=loss.item()*batch_size
+
+        #caluclate  the accuracy since binary classification
+        pred =(torch.sigmoid(logits)>=0.5).float()
+        total_correct+=(pred==y_batch).sum().item()
+
+        # count all samples 
+        total_sample +=batch_size
+
+    # find teh average loss for the enitre data set 
+    average_loss =train_loss/total_sample 
+    average_acc =total_correct/total_sample
+
+    return  average_loss,average_acc
+    
+
+
+
+
+
+
+def test_setup(model,test_dataloader,loss_fn):
+    model.eval()
+    test_loss =0
+    test_correct =0
+    total_sample =0
+    with torch.inference_mode():
+        for X_batch,y_batch in test_dataloader:
+             logits =model(X_batch).squeeze(1)
+
+             loss =loss_fn(logits,y_batch)
+             # calulate  sample per batch
+             batch_size =X_batch.size(0)
+
+             test_loss +=loss.item()*batch_size
+             pred =(torch.sigmoid(logits)>=0.5).float()
+             test_correct+=(pred==y_batch).sum().item()
+             total_sample+=batch_size
+    aver_test_loss =test_loss/total_sample
+    aver_test_acc =test_correct/total_sample
+    return aver_test_loss,aver_test_acc
+
+def train(model,train_dataloader,test_dataloader,loss_fn,optimizer):
+    epochs =100
+    #store metrics
+    result = [
+        "train_loss" =[],
+        "test_loss "=[],
+        "train_acc"=[],
+        "test_acc"=[]   ]
+    for epoch in range(epochs):
+        train_loss,train_acc =train_setup(model=model,train_dataloader=train_dataloader,loss_fn =loss_fn,optimizer=optimizer)
+
+        test_loss,test_acc =test_setup(model =model,test_dataloader=test_dataloader,loss_fn=loss_fn)
+
+        result["train_loss"].append(train_loss)
+        result["test_loss"].append(test_loss)
+        result["train_acc"].append(train_acc)
+        result["test_acc"].append(test_acc)
+        # log all epoch metric to mlflow 
+        mlflow.log_metrics ({
+            "train_loss":train_loss,
+            "test_loss":test_loss,
+            "train_accuracy":train_acc,
+            "test_accuracy ":test_acc},step =epoch
+        )
+        print(f"the number of epochs are:{epoch}")
+        print(f"the train accuracry is :{train_acc}")
+        print(f"the test accuracy is :{test_acc}")
+        print(f"the train loss is {train_loss}")
+        print(f"the test loss is{test_loss}")
+    return result
