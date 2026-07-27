8930e4fa14469a3055d65dbaee71fc26de7df9c6
GMG <geejamolg@gmail.com>
2026-07-27 15:38:35 +0400

    added mlflow component (basic)

--- going_modular/train.py
+++ going_modular/train.py
@@ -3,7 +3,11 @@ from datasetup import data_download
 from datasetup import check_data_quality
 from datasetup import split_balance
 import torch
+from torch import nn
 from model import Classgitmodel
+from dataloader import dataloader
+from engine import train
+import mlflow 
 
 data_path = data_download(
     url="https://archive.ics.uci.edu/static/public/350/data.csv",
@@ -29,33 +33,47 @@ print(f"the shape of y_train: {y_train.shape}")
 print(f"the shape of X-test:{X_test.shape}")
 print(f"the shape of X-test:{y_test.shape}")
 
-#convert to tensor
-X_train_tensor = torch.tensor(
-    X_train.to_numpy(),
-    dtype=torch.float32
-)
+train_dataloader,test_dataloader ,input_feature=dataloader(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test)
+X_batch, y_batch = next(iter(train_dataloader))
 
-X_test_tensor = torch.tensor(
-    X_test.to_numpy(),
-    dtype=torch.float32
-)
-
-y_train_tensor = torch.tensor(
-    y_train.to_numpy(),
-    dtype=torch.float32
-)
-
-y_test_tensor = torch.tensor(
-    y_test.to_numpy(),
-    dtype=torch.float32
-
-)
-
+print("Features:", X_batch.shape)
+print("Targets:", y_batch.shape)
 # call the model 
-input_feature =X_train_tensor.shape[1]
+#input_feature =X_train_tensor.shape[1]
 hidden_feature =32
 output_feature =1
 model = Classgitmodel(input_features=input_feature,
                       hidden_features=hidden_feature,
                       output_feature=output_feature)
 print(f"model :{model.state_dict()}")
+loss_fn = nn.BCEWithLogitsLoss()
+
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=0.001
+)
+mlflow.set_experiement("Binary Classification ")
+epo =100
+mlflow.enable_system_metrics_logging()
+with mlflow.start_run():
+    # lof the parameters
+    mlflow.log_params({
+        "EPOCHS":epo,
+        "OPTIMIZER ":optimizer.__class__.__name__,
+        "LOSS_FUNCTION ":loss_fn.__class__.__name__,
+        "MODEL_NAME":model.__class__.__name__,
+        "LEARNING RATE":0.001,
+        "INPUT_FEATURE ":input_feature,
+        "HIDDEN_FEATURE":hidden_feature,
+        "OUTPUT_FEATURE":output_feature,
+
+    })
+    result =train(model=model,
+                  train_dataloader=train_dataloader,
+                  test_dataloader=test_dataloader,
+                  loss_fn=loss_fn,
+                  optimizer =optimizer,epoch=epo)
+    mlflow.pytorch.log_model(
+        pytorch_model=model,
+        name="BINARY CLASSIFIFER"
+    )
